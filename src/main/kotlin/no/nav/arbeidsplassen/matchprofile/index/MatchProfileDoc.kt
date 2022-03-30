ced604cfb9cc8d4b1efcd16843eb294f64d659d4
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2022-03-30 09:53:05 +0200

    fix locations

--- src/main/kotlin/no/nav/arbeidsplassen/matchprofile/index/MatchProfileDoc.kt
+++ src/main/kotlin/no/nav/arbeidsplassen/matchprofile/index/MatchProfileDoc.kt
@@ -4,13 +4,13 @@ import java.time.Instant
 
 data class MatchProfileDoc(val id: String, val orgnr: String?, val sourceId: String, val type: String, val status: String,
                            val title: String?, val description: String?, val concepts: List<String> = listOf(),
-                           val country: List<String?> = listOf(), val county: List<String?> = listOf(), val municipal: List<String?> = listOf(),
+                           val country: List<String?> = listOf(), val county: List<String?> = listOf(), val municipality: List<String?> = listOf(),
                            val city: List<String?> = listOf(), val createdBy: String, val updatedBy: String,
                            val expires: Instant, val created: Instant, val updated: Instant)
 
 fun MatchProfileDTO.toDoc() = MatchProfileDoc(id = id, orgnr = orgnr, sourceId = sourceId, type = type, status = status, title = title, description = description,
     concepts = profile.concepts.map { it.label }, country = profile.locations.map { it.country },
-    county = profile.locations.map { it.county }, municipal = profile.locations.map { it.municipal },
+    county = profile.locations.map { it.county }, municipality = profile.locations.map { it.municipality },
     createdBy = createdBy, updatedBy = updatedBy, expires = expires, created = created, updated = updated)
 
 
